172c748a07b167b29acd2db968706ab0a47229e8
StineMartinsen <80045646+StineMartinsen@users.noreply.github.com>
2022-06-16 17:35:59 +0200

    fiksa litt på features

---
+++ examples/features/features.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+from tkinter import LEFT, RIGHT
+from turtle import down
+
+import Pyro4
+
+from edurov import WebMethod
+
+
+import RPi.GPIO as GPIO
+
+#set GPIO numbering mode and define output pins
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4,GPIO.OUT) #UO
+GPIO.setup(5,GPIO.OUT) #DOWN
+GPIO.setup(6,GPIO.OUT) #RIGTH
+GPIO.setup(26,GPIO.OUT) #LEFT
+
+UP = False
+DOWN = False
+RIGHT = False
+LEFT =False
+
+
+def control_motors():
+    with Pyro4.Proxy("PYRONAME:KeyManager") as keys:
+        with Pyro4.Proxy("PYRONAME:ROVSyncer") as rov:
+            while rov.run:
+                if keys.state('K_UP'):
+                    print('Forward')
+                elif keys.state('K_DOWN'):
+                    UP = True
+                else:
+                    UP = False
+                if keys.state('K_DOWN'):
+                    print('Backward')
+                    DOWN = True
+                else:
+                    DOWN = False
+                if keys.state('K_RIGHT'):
+                    RIGHT = True
+                else:
+                    RIGHT = False
+                if keys.state('K_RIGHT'):
+                    print('Right')
+                    RIGHT = True
+                if keys.state('K_LEFT'):
+                    LEFT = True
+                else:
+                    LEFT = False
+                if keys.state('K_LEFT'):
+                    print('left')
+
+                    LEFT = True
+                else:
+                    LEFT = False
+
+                GPIO.output(4,UP)
+                GPIO.output(5,DOWN)
+                GPIO.output(6,RIGHT)
+                GPIO.output(26,LEFT)
+
+
+# Create the WebMethod class
+web_method = WebMethod(
+    index_file=os.path.join(os.path.dirname(__file__), 'index.html'),
+    runtime_functions=control_motors,
+    custom_response=my_response
+)
+# Start serving the web page, blocks the program after this point
+web_method.serve()
