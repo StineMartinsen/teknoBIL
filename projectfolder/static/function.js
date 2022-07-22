47519a47a09ad0dbc32e5d50357b2c21a03b855e
PeterhdPham <peterhdpham@gmail.com>
2022-07-22 12:16:43 +0200

    k

--- projectfolder/static/function.js
+++ projectfolder/static/function.js
@@ -5,7 +5,7 @@ $(function () {
       //Do nothing
     });
     var url = $("#re").attr("src");
-    $("##re").removeAttr("src").attr("src", url);
+    $("#re").removeAttr("src").attr("src", url);
     return false;
   });
 });
